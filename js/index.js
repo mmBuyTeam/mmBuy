3bf3d11d688bb5de8fe8714044bc6eb3ca0cdd46
songwei <340042089@qq.ocm>
2017-03-16 17:30:13 +0800

    add files: category

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
 window.onload = function (){
-	new Vue({
-		el:'#nav',
-        data:{
-            navData: [],
-        },
-        mounted:function(){
-        	this.getData();
-        },
-        methods:{
-            getData:function(){
-                var self = this;
-                this.$http.jsonp('http://mmb.ittun.com/api/getindexmenu')
-                .then(function(res){
-                    console.log(res.data);
-                },function(res){
-                    console.log(res.status);
-                });
-            }
-        }
+	// 导航栏加载
+	$.ajax({
+		type: 'get',
+		data: '{}',
+		async: false,
+		url: 'http://192.168.15.164:3000/api/getindexmenu',
+		dataType: 'jsonp',
+		success: function(data){
+			var html = template('tmp',data);
+			$('#nav').html(html);
+			var count = 1;
+			$('#nav>ul>li').eq(7).on('click',function(){
+				// .toggle('flex')
+				count++;
+				if(count%2 != 1){
+					$('#nav ul li:nth-child(n+9)').css('display','flex');
+				}else{
+					$('#nav ul li:nth-child(n+9)').css('display','none');
+				}
+			});
+		}
 	});
+	$.ajax({
+		type: 'get',
+		data: '{}',
+		async: false,
+		url: 'http://192.168.15.164:3000/api/getmoneyctrl',
+		dataType: 'jsonp',
+		success: function(data){
+			var html = template('tem',data);
+			$('.goods').find('h3').eq(0).after(html);
+		}
+	});
+
 }
